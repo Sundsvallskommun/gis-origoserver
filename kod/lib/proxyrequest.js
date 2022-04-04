799fa6056e1e88d9cb17f655959ac660f8812904
Blästa Johnny <johnny.blasta@sundsvall.se>
2022-04-04 16:17:55 +0200

    Test remove headers

--- kod/lib/proxyrequest.js
+++ kod/lib/proxyrequest.js
@@ -1,12 +1,12 @@
 var request = require('request');
 
 module.exports = function proxyRequest(req, res, options) {
-  if (!res.header('Access-Control-Allow-Origin')) {
+  /*if (!res.header('Access-Control-Allow-Origin')) {
     res.header("Access-Control-Allow-Origin", "*");
   }
   if (!res.header('Access-Control-Allow-Headers')) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  }
+  }*/
   req.pipe(request.get(options))
   .on('error', err => {
     const msg = 'Error!';
