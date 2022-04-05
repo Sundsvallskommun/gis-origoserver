2ee2e5e9c44d778c5cbbca1f59ddd7aefe3d7a57
Blästa Johnny <johnny.blasta@sundsvall.se>
2022-04-05 08:52:58 +0200

    Test

--- kod/lib/proxyrequest.js
+++ kod/lib/proxyrequest.js
@@ -1,17 +1,17 @@
 var request = require('request');
 
 module.exports = function proxyRequest(req, res, options) {
-  /*if (!res.header('Access-Control-Allow-Origin')) {
+  if (!res.header('Access-Control-Allow-Origin')) {
     res.header("Access-Control-Allow-Origin", "*");
   }
   if (!res.header('Access-Control-Allow-Headers')) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  }*/
+  }
   req.pipe(request.get(options))
   .on('error', err => {
     const msg = 'Error!';
     console.error(msg, err);
-    res.status(200).send(msg);
+    res.status(500).send(msg);
   })
   .pipe(res);
 }
