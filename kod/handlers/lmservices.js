5474d0cd405bedaa96d847031ba29e9c05ff353e
Johnny Blästa <johnny.blasta@sundsvall.se>
2025-10-28 09:22:46 +0100

    Param check

--- kod/handlers/lmservices.js
+++ kod/handlers/lmservices.js
@@ -31,198 +31,202 @@ const lmservices = async function lmservices(params = {}) {
     includeData = 'total'
   } = params;
   let returnObj = {};
-  if (conf && conf.lmservices && conf.lmservices.services && conf.lmservices.services[typ]) {
-    const serviceConf = conf.lmservices.services[typ];
-    if (conf.lmservices.apps && conf.lmservices.apps[serviceConf.app]) {
-      const appConf = conf.lmservices.apps[serviceConf.app];
-      const tokenObject = await lmtokenhandler(appConf);
-      if (tokenObject) {
-        if (typ === 'belagenhetsadress') {
-          if (fastighet) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/registerenhet/${fastighet}?includeData=${includeData}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            returnObj.belagenhetsadress = response;
-          }
-        }
-        if (typ === 'byggnad') {
-          let ids = id;
-          if (fastighet) {
-            ids = await makeRequest({
-              url: `${serviceConf.url}/referens/beror/${fastighet}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-          }
-          if (ids && ids.length > 0) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/?includeData=${includeData}`,
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              },
-              data: ids
-            }).catch(handleError);
-            returnObj.byggnad = response;
-          }
-        }
-        if (typ === 'fastighetsamfallighet') {
-          if (fastighet) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/${fastighet}?includeData=${includeData}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            returnObj.fastighetsamfallighet = response;
-          }
-        }
-        if (typ === 'gemensamhetsanlaggning') {
-          let ids = id;
-          if (fastighet) {
-            const refs = await makeRequest({
-              url: `${serviceConf.url}/referens/delagande/${fastighet}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            if (refs) {
-              ids = refs.map((ref) => ref.objektidentitet)
+  const checkUuidRegEx = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/i;
+  let found = fastighet.match(checkUuidRegEx);
+  if (found !== null) {
+    if (conf && conf.lmservices && conf.lmservices.services && conf.lmservices.services[typ]) {
+      const serviceConf = conf.lmservices.services[typ];
+      if (conf.lmservices.apps && conf.lmservices.apps[serviceConf.app]) {
+        const appConf = conf.lmservices.apps[serviceConf.app];
+        const tokenObject = await lmtokenhandler(appConf);
+        if (tokenObject) {
+          if (typ === 'belagenhetsadress') {
+            if (fastighet) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/registerenhet/${fastighet}?includeData=${includeData}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              returnObj.belagenhetsadress = response;
             }
           }
-          if (ids && ids.length > 0) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/?includeData=${includeData}`,
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              },
-              data: ids
-            }).catch(handleError);
-            if (response.features?.length > 0) {
-              for (const ga of response.features) {
-                const response2 = await lmservices({ typ: 'rattighet', fastighet: ga.id });
-                if (response2.rattighet?.features?.length > 0) {
-                  ga.rattighet = response2.rattighet;
+          if (typ === 'byggnad') {
+            let ids = id;
+            if (fastighet) {
+              ids = await makeRequest({
+                url: `${serviceConf.url}/referens/beror/${fastighet}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+            }
+            if (ids && ids.length > 0) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/?includeData=${includeData}`,
+                method: 'POST',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                },
+                data: ids
+              }).catch(handleError);
+              returnObj.byggnad = response;
+            }
+          }
+          if (typ === 'fastighetsamfallighet') {
+            if (fastighet) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/${fastighet}?includeData=${includeData}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              returnObj.fastighetsamfallighet = response;
+            }
+          }
+          if (typ === 'gemensamhetsanlaggning') {
+            let ids = id;
+            if (fastighet) {
+              const refs = await makeRequest({
+                url: `${serviceConf.url}/referens/delagande/${fastighet}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              if (refs) {
+                ids = refs.map((ref) => ref.objektidentitet)
+              }
+            }
+            if (ids && ids.length > 0) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/?includeData=${includeData}`,
+                method: 'POST',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                },
+                data: ids
+              }).catch(handleError);
+              if (response.features?.length > 0) {
+                for (const ga of response.features) {
+                  const response2 = await lmservices({ typ: 'rattighet', fastighet: ga.id });
+                  if (response2.rattighet?.features?.length > 0) {
+                    ga.rattighet = response2.rattighet;
+                  }
+                }
+              }
+              returnObj.gemensamhetsanlaggning = response;
+            }
+          }
+          if (typ === 'inskrivning') {
+            if (fastighet) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/beror/${fastighet}?includeData=${includeData}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              returnObj.inskrivning = response;
+            }
+          }
+          if (typ === 'markreglerandebestammelse') {
+            let ids = id;
+            if (fastighet) {
+              const refs = await makeRequest({
+                url: `${serviceConf.url}/referens/beror/${fastighet}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              if (refs) {
+                ids = refs.map((ref) => ref.objektidentitet)
+              }
+            }
+            if (ids && ids.length > 0) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/?includeData=${includeData}`,
+                method: 'POST',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                },
+                data: ids
+              }).catch(handleError);
+              returnObj.markreglerandebestammelse = response;
+            }
+          }
+          if (typ === 'rattighet') {
+            let ids;
+            if (id) {
+              ids = { "id": id }
+            }
+            if (fastighet) {
+              const refs = await makeRequest({
+                url: `${serviceConf.url}/referens/beror/${fastighet}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              if (refs) {
+                ids = {
+                  "id": refs.map((ref) => ref.objektidentitet)
                 }
               }
             }
-            returnObj.gemensamhetsanlaggning = response;
-          }
-        }
-        if (typ === 'inskrivning') {
-          if (fastighet) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/beror/${fastighet}?includeData=${includeData}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            returnObj.inskrivning = response;
-          }
-        }
-        if (typ === 'markreglerandebestammelse') {
-          let ids = id;
-          if (fastighet) {
-            const refs = await makeRequest({
-              url: `${serviceConf.url}/referens/beror/${fastighet}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            if (refs) {
-              ids = refs.map((ref) => ref.objektidentitet)
+            if (ids && ids.id && ids.id.length > 0) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/?includeData=${includeData}`,
+                method: 'POST',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                },
+                data: ids
+              }).catch(handleError);
+              returnObj.rattighet = response;
             }
           }
-          if (ids && ids.length > 0) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/?includeData=${includeData}`,
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              },
-              data: ids
-            }).catch(handleError);
-            returnObj.markreglerandebestammelse = response;
-          }
-        }
-        if (typ === 'rattighet') {
-          let ids;
-          if (id) {
-            ids = { "id": id }
-          }
-          if (fastighet) {
-            const refs = await makeRequest({
-              url: `${serviceConf.url}/referens/beror/${fastighet}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            if (refs) {
+          if (typ === 'taxering') {
+            let ids;
+            if (id && id.length > 0) {
               ids = {
-                "id": refs.map((ref) => ref.objektidentitet)
+                "taxeringsenhetsnummer": id
               }
             }
-          }
-          if (ids && ids.id && ids.id.length > 0) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/?includeData=${includeData}`,
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              },
-              data: ids
-            }).catch(handleError);
-            returnObj.rattighet = response;
-          }
-        }
-        if (typ === 'taxering') {
-          let ids;
-          if (id && id.length > 0) {
-            ids = {
-              "taxeringsenhetsnummer": id
-            }
-          }
-          if (fastighet) {
-            const refs = await makeRequest({
-              url: `${serviceConf.url}/referens/beror/${fastighet}`,
-              method: 'GET',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              }
-            }).catch(handleError);
-            if (refs) {
-              ids = {
-                "taxeringsenhetsnummer": refs.taxeringsenhetsreferens.map((ref) => ref.taxeringsenhetsnummer)
+            if (fastighet) {
+              const refs = await makeRequest({
+                url: `${serviceConf.url}/referens/beror/${fastighet}`,
+                method: 'GET',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                }
+              }).catch(handleError);
+              if (refs) {
+                ids = {
+                  "taxeringsenhetsnummer": refs.taxeringsenhetsreferens.map((ref) => ref.taxeringsenhetsnummer)
+                }
               }
             }
+            if (ids && ids.taxeringsenhetsnummer && ids.taxeringsenhetsnummer.length > 0) {
+              const response = await makeRequest({
+                url: `${serviceConf.url}/?includeData=${includeData}`,
+                method: 'POST',
+                headers: {
+                  'Authorization': `Bearer ${tokenObject.token}`
+                },
+                data: ids
+              }).catch(handleError);
+              returnObj.taxering = response;
+            }
           }
-          if (ids && ids.taxeringsenhetsnummer && ids.taxeringsenhetsnummer.length > 0) {
-            const response = await makeRequest({
-              url: `${serviceConf.url}/?includeData=${includeData}`,
-              method: 'POST',
-              headers: {
-                'Authorization': `Bearer ${tokenObject.token}`
-              },
-              data: ids
-            }).catch(handleError);
-            returnObj.taxering = response;
-          }
-        }
-      } else { returnObj = { error: 'No token' } }
-    } else { returnObj = { error: 'Error in configuration' } }
-  } else { returnObj = { error: 'Error in configuration. Typ missing' } }
+        } else { returnObj = { error: 'No token' } }
+      } else { returnObj = { error: 'Error in configuration' } }
+    } else { returnObj = { error: 'Error in configuration. Typ missing' } }
+  } else { returnObj = { error: 'Error fastighet parameter not valid' } }
   return returnObj;
 }
 module.exports = lmservices;
